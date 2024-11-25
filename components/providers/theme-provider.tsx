2ca1e17f34c1fd5aba6a11cb99e213e670f85803
RakeenS <156736795+RakeenS@users.noreply.github.com>
2024-11-25 16:50:25 -0600

    Fix theme provider type imports

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemesProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemesProviderProps } from "next-themes"
 
 interface ThemeProviderProps extends Omit<NextThemesProviderProps, "attribute"> {
   children: React.ReactNode
